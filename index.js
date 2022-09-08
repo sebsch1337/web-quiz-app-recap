a2db5f3c8c9110a87a09530ca36068a71dbb3bd3
Sebastian Scherbes <github@scherbes.de>
2022-09-08 10:40:33 +0200

    add toggle functionality to answer-button

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
 
-console.log(bookmarkButton);
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const cardAnswer = document.querySelector('[data-js="card-answer"]');
 
 bookmarkButton.addEventListener("click", () => {
   bookmarkIcon.classList.toggle("bookmark--active");
+});
+
+answerButton.addEventListener("click", () => {
+  if (answerButton.textContent === "Hide answer") {
+    answerButton.textContent = "Show answer";
+  } else {
+    answerButton.textContent = "Hide answer";
+  }
+  cardAnswer.classList.toggle("card__answer--active");
 });
